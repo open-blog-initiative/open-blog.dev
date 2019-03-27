d136e2e3d8647fa6b520bd680ceebc4f31fa3ded
Slashgear <antoine395.caron@gmail.com>
2019-03-27 08:50:18 +0100

    docs(post): add first post in fr

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,52 @@
 module.exports = {
-  title: "Open blog"
+  title: "Open blog",
+  plugins: {
+    "@vuepress/pwa": {
+      serviceWorker: true,
+      updatePopup: {
+        message: "New content is available.",
+        buttonText: "Refresh"
+      }
+    }
+  },
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Open blog",
+      description: "Share blog posts on an free open platform"
+    },
+    "/fr/": {
+      lang: "fr-FR",
+      title: "Blog libre",
+      description: "Partage tes articles sur une plateforme libre"
+    }
+  },
+  themeConfig: {
+    locales: {
+      "/": {
+        selectText: "Languages",
+        label: "English",
+        editLinkText: "Edit this page on GitHub",
+        lastUpdated: "Last Updated",
+        repo: "open-blog-initiative/open-blog.dev",
+        repoLabel: "Contribute!",
+        docsRepo: "open-blog-initiative/open-blog.dev",
+        docsDir: "docs",
+        editLinks: true,
+        sidebar: "auto"
+      },
+      "/fr/": {
+        selectText: "Langues",
+        label: "Français",
+        editLinkText: "Edit cette page sur Github",
+        lastUpdated: "Mis à jour le",
+        repo: "open-blog-initiative/open-blog.dev",
+        repoLabel: "Contribue !",
+        docsRepo: "open-blog-initiative/open-blog.dev",
+        docsDir: "docs",
+        editLinks: true,
+        sidebar: "auto"
+      }
+    }
+  }
 };
