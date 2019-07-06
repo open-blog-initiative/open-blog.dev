8dcba51f59ed71a07ebb64a4af57da59f4fab72d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 14:39:43 +0000

    Deploy open-blog.dev to github.com/open-blog-initiative/open-blog.dev.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3a6334d042ad9abc9b97.js"
+    "url": "webpack-runtime-05e33b1c885dfe67aed9.js"
   },
   {
     "url": "styles.a0772ad69aa4eeb5fd04.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c80ddf8465d03c88bb5b33261a1f0ffc"
+    "revision": "ebb49c9af6f774ac20884b82c5537826"
   },
   {
     "url": "component---src-pages-404-js-ed2df8c4c397e23a1a6b.js"
